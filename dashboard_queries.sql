642fbb13c40fc3ce7e34ccc626350d8500e47d5d
Erik Martiš <161586667+Programatoris@users.noreply.github.com>
2024-12-30 17:56:03 +0100

    Update dashboard_queries.sql fixed typos

--- dashboard_queries.sql
+++ dashboard_queries.sql
@@ -23,12 +23,11 @@ ORDER BY year;
 
 --4. Average Gross Income vs. Average Rating by Genre
 SELECT 
-    g.genre, 
+    m.genre, 
     ROUND(AVG(m.worldwide_gross_income), 2) AS avg_income, 
     ROUND(AVG(f.avg_rating), 2) AS avg_rating
 FROM FACT_MOVIE f
 JOIN DIM_MOVIE m ON f.fact_movie_id = m.dim_movie_id
-JOIN genre_staging g ON m.dim_movie_id = g.movie_id
 GROUP BY g.genre
 ORDER BY avg_income DESC;
 
@@ -40,7 +39,6 @@ SELECT
     COUNT(*) AS movie_count
 FROM FACT_MOVIE f
 JOIN DIM_PERSON p ON f.person_id = p.dim_person_id
-JOIN director_mapping_staging dms ON p.dim_person_id = dms.name_id
 GROUP BY p.name
 ORDER BY avg_rating DESC
 LIMIT 5;
@@ -50,9 +48,9 @@ LIMIT 5;
 SELECT 
     m.title, 
     m.duration, 
-    r.avg_rating
+    f.avg_rating
 FROM DIM_MOVIE m
-JOIN ratings_staging r ON m.dim_movie_id = r.movie_id
+JOIN FACT_MOVIE f ON m.dim_movie_id = f.fact_movie_id
 WHERE m.duration IS NOT NULL 
 AND r.avg_rating IS NOT NULL
 ORDER BY m.duration DESC
